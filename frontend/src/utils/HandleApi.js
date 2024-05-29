9c44dc8db7a7a73ee09d24a2937cc2efe06264a6
Shadow007sayan <96434944+Shadow007sayan@users.noreply.github.com>
2024-05-29 13:03:28 +0530

    Update HandleApi.js

--- frontend/src/utils/HandleApi.js
+++ frontend/src/utils/HandleApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = "http://localhost:8080"
+const baseUrl = "https://todo-app-react-mongo-db-deploy-on-verce-atlas-server.vercel.app"
 
 const getAllToDo =(setToDo)=>{
     axios.get(baseUrl).then(({data})=>{
